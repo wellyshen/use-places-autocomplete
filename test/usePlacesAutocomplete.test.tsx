2000566921c7570f02673e00f02c71615bdef859
Welly Shen <hivoid19@gmail.com>
2020-01-30 17:21:42 +0800

    Test: add test cases

--- test/usePlacesAutocomplete.test.tsx
+++ test/usePlacesAutocomplete.test.tsx
@@ -3,7 +3,19 @@ import { renderHook } from '@testing-library/react-hooks';
 import usePlacesAutocomplete from '../src/usePlacesAutocomplete';
 
 describe('usePlacesAutocomplete', () => {
-  it('should...', () => {
-    renderHook(usePlacesAutocomplete);
+  it('should assign the init() as the global variable of Google script callback', () => {
+    // TODO: test API loaded case
+    const callbackName = 'initMap';
+    renderHook(() => usePlacesAutocomplete({ callbackName }));
+    expect((window as any)[callbackName]).toEqual(expect.any(Function));
+  });
+
+  it('should delete the global variable of Google script callback when un-mount', () => {
+    const callbackName = 'initMap';
+    const { unmount } = renderHook(() =>
+      usePlacesAutocomplete({ callbackName })
+    );
+    unmount();
+    expect((window as any)[callbackName]).toBeUndefined();
   });
 });
