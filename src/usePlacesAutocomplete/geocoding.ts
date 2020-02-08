a22a93bf80b87d495f993406bfab72b70809700e
Welly Shen <hivoid19@gmail.com>
2020-02-09 01:40:06 +0800

    Feat: add geocoding utils

---
+++ src/usePlacesAutocomplete/geocoding.ts
@@ -0,0 +1,32 @@
+interface GeoArgs {
+  address?: string;
+  placeId?: string;
+}
+type GeocodeResult = google.maps.GeocoderResult;
+type GeoReturn = Promise<GeocodeResult[]>;
+
+export const getGeocode = ({ address, placeId }: GeoArgs): GeoReturn => {
+  const geocoder = new window.google.maps.Geocoder();
+
+  return new Promise((resolve, reject) => {
+    geocoder.geocode({ address, placeId }, (results, status) => {
+      if (status !== 'OK') reject(status);
+
+      resolve(results);
+    });
+  });
+};
+
+type LatLng = { lat: number; lng: number };
+type LatLngReturn = Promise<LatLng>;
+
+export const getLatLng = (result: GeocodeResult): LatLngReturn =>
+  new Promise((resolve, reject) => {
+    try {
+      const { lat, lng } = result.geometry.location;
+
+      resolve({ lat: lat(), lng: lng() });
+    } catch (error) {
+      reject(error);
+    }
+  });
